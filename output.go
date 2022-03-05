f835778fd9553eac3ab5526fc76c3e719d30096b
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2022-03-05 23:16:39 +0900

    prac some print functions

--- output.go
+++ output.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+
+	// デフォルトフォーマット改行あり
 	fmt.Println("hello golang")
-	fmt.Println(1 + 3)
+	// 書き込み先を明示的に指定
+	fmt.Fprintln(os.Stdout, "go lang")
+
+	// フォーマットした結果を文字列で返す
+	hello := fmt.Sprint("Hello Wolrd")
+	fmt.Println(hello)
+
+	fmt.Printf("%#v\n", hello)
 }
