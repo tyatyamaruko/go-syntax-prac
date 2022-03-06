d77966207d89c6d714b60e5a843285782e323f82
ryotatanks9981 <67944448+ryotatanks9981@users.noreply.github.com>
2022-03-06 17:28:50 +0900

    pointer lesson2

--- pointer.go
+++ pointer.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var a1 int
-	var p1 *int
+	var a1 int = 123
+	var a2 int = 123
+	// var p1 *int
 
-	p1 = &a1
-	*p1 = 123
+	// p1 = &a1
+	// *p1 = 123
+	fn(a1, &a2)
+	fmt.Println(a1, a2)
+}
 
-	fmt.Println(a1)
+func fn(b1 int, b2 *int) {
+	b1 = 456
+	*b2 = 456
 }
